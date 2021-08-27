6c9362954ff88578244f34632241e3919312771e
Hemant Rakesh <basketballlfe600@gmail.com>
2021-08-28 03:47:21 +0530

    script to setup project

---
+++ doc_layout/setup.sh
@@ -0,0 +1,6 @@
+pip install -r requirements.txt
+pip install -U layoutparser
+git clone https://github.com/Layout-Parser/layout-parser.git
+cd layout-parser/
+sudo apt install tesseract-ocr
+python -m spacy download en_core_web_trf
