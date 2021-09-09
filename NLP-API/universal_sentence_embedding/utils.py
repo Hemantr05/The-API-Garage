5cc303b49d455923782d264eea7a6ffcd437518d
Hemant Rakesh <basketballlfe600@gmail.com>
2021-09-09 20:46:16 +0530

    added logging

--- NLP-API/universal_sentence_embedding/utils.py
+++ NLP-API/universal_sentence_embedding/utils.py
@@ -1,5 +1,5 @@
-from absl import logging
-
+# from absl import logging
+import logging
 import tensorflow as tf
 
 import tensorflow_hub as hub
@@ -9,13 +9,16 @@ import os
 import pandas as pd
 import re
 import seaborn as sns
+logging.basicConfig(filename='compile.log', level=logging.DEBUG)
 
 def USE(text):
     response = dict()
     module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
     model = hub.load(module_url)
+    logging.info('Loaded USE module')
     messages = [text]
     message_embeddings = model(messages)
+    logging.info(f'successfully generated embeddings of length {len(message_embeddings)}')
     response['sentence/text'] = text
     response['embeddings'] = message_embeddings
     return response
