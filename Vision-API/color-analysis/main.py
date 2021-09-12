9b8736ca3c6732114d4b4dc1b80ef31eb8b2c15c
Hemant Rakesh <basketballlfe600@gmail.com>
2021-09-12 18:45:09 +0530

    modified response to json

--- Vision-API/color-analysis/main.py
+++ Vision-API/color-analysis/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
 
-from utils import prep_image, color_analysis
+from utils import prep_image, color_analysis, save_upload_file_tmp
 
 
 app = FastAPI()
@@ -16,16 +16,8 @@ async def analyze(file: UploadFile = File(...)):
 
     filename = save_upload_file_tmp(file, "uploads")
 
-    # Check for file type (image or video)
-
-    # For image
     preprocess_image = prep_image(filename)
-    color_analysis(preprocess)
-    # return StreamingResponse(predicted, media_type="image/png")
-
-
-    # TODO: read and return predicted video file .
-    # For video
-
+    colors = color_analysis(preprocess)
+    return colors
 
     # Download or send response as image or video with predictions
